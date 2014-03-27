dff72c9b3b3f0dd4a4662febfe1dbcbdd5c0e7a6
Vitaliy Gerya <vgerya@skytide.com>
2014-03-27 14:14:42 +0200

    Initial commit.

---
+++ test/src/com/mype/homesync/service/SyncronizerServiceShould.java
@@ -0,0 +1,14 @@
+package com.mype.homesync.service;
+
+import org.junit.Test;
+
+/**
+ * @author Vitaliy Gerya
+ */
+public class SyncronizerServiceShould {
+    @Test
+    public void shouldAuthenticateBT() throws Exception {
+
+    }
+
+}
